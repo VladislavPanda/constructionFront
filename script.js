9acdea8849d068b06f40d99e11d90791898fa21c
VladislavS999 <phpmailer1999@gmail.com>
2022-04-02 00:48:42 +0300

    Создан обработчик формы заявок

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ const HelloVueApp = {
   }
   
   Vue.createApp(HelloVueApp).mount('#hello-vue')
+
+const url = 'http://construction/bid/create';
+const infoWindow = document.getElementById('info');
+
+async function handler() {
+  const inputData = Array.from(document.querySelectorAll('input, select, textarea'))
+    .map(element  => element.value)
+    
+  const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(inputData),
+      headers: {
+        'Content-Type': 'text/plain;charset=UTF-8'
+      }
+    }).catch((e) => {
+      infoWindow.innerText = 'Ошибка запроса: \n' + e;
+      throw e;
+    });
+    
+    try {
+      const data = await response.text();
+      infoWindow.innerText = `Ответ сервера:\n${data}`;
+    } catch(e) {
+      infoWindow.innerText = 'Ошибка обработки ответа с сервера: \n' + e;
+      throw e;
+    }			
+}
